0579368a7f1ebcb2ebde679b6dac14366546717c
philippe-hjik <122774959+philippe-hjik@users.noreply.github.com>
2024-09-25 10:47:04 +0200

    Update Program.cs

--- personnel/maximum/maximumGame/maximumGame/Program.cs
+++ personnel/maximum/maximumGame/maximumGame/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 4 players
+List<Player> players = new List<Player>()
+{
+    new Player("Joe", 32),
+    new Player("Jack", 30),
+    new Player("William", 37),
+    new Player("Averell", 25)
+};
+
+List<Player> playersOrder = players.OrderByDescending(i => i.Age).ToList();
+
+// Initialize search
+Player elder = players.First();
+int biggestAge = elder.Age;
+
+Console.WriteLine($"Le plus agé est {elder.Name} qui a {elder.Age} ans");
+
+Console.ReadKey();
+
+public class Player
+{
+    private readonly string _name;
+    private readonly int _age;
+
+    public Player(string name, int age)
+    {
+        _name = name;
+        _age = age;
+    }
+
+    public string Name => _name;
+
+    public int Age => _age;
+}
